45e34b1964115a4df5ec94bd5ecc996c5ac70ca6
tiptop <Tomasz.Gasior@gmail.com>
2020-09-18 16:19:05 +0200

    Initial commit - route showing simple session summary.

---
+++ lib/gui/session_summary_route.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:math_puzzles/gui/math_puzzle.dart';
+import 'package:math_puzzles/localizations.dart';
+
+import '../model.dart';
+
+class SessionSummaryRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var _sessionModel =
+        ModalRoute.of(context).settings.arguments as SessionModel;
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(AppLocalizations.of(context).sessionSummary),
+          leading: null,
+          automaticallyImplyLeading: false,
+          actions: createActions(context),
+        ),
+        body: Column(
+          children: <Widget>[
+            _createCorrectAnswersSummary(context, _sessionModel.correctAnswersCount),
+            _createIncorrectAnswersSummary(context, _sessionModel.incorrectAnswersCount),
+          ],
+        ));
+  }
+
+  Widget _createCorrectAnswersSummary(BuildContext context, int correctAnswersCount) {
+    return Text('${AppLocalizations.of(context).correctAnswersCount} $correctAnswersCount');
+  }
+
+  Widget _createIncorrectAnswersSummary(BuildContext context, int incorrectAnswersCount) {
+    return Text('${AppLocalizations.of(context).incorrectAnswersCount} $incorrectAnswersCount');
+  }
+
+}
