685c24ad82b92882ca87372e3ed326860cbae9a7
tiptop <Tomasz.Gasior@gmail.com>
2020-10-30 17:16:33 +0100

    Present settings on ListView.

--- lib/gui/settings_route.dart
+++ lib/gui/settings_route.dart
@@ -9,130 +9,92 @@ class SettingsRoute extends StatefulWidget {
   SettingsRoute(this._configuration);
 
   @override
-  _SettingsRouteState createState() => _SettingsRouteState();
+  State createState() {
+    return SettingsRouteState();
+  }
 }
 
-class _SettingsRouteState extends State<SettingsRoute> {
-  final Map<String, TextEditingController> _editingControlers = {};
-  final Map<String, String> _errorMessages = {};
-
-  @override
-  void dispose() {
-    _editingControlers.forEach((key, value) => value.dispose());
-    super.dispose();
-  }
+class SettingsRouteState extends State<SettingsRoute> {
+  static const String sectionDivider = '.';
 
   @override
   Widget build(BuildContext context) {
+    var groupedParams = _prepareListItems(widget._configuration.parameters);
     return Scaffold(
-        appBar: AppBar(automaticallyImplyLeading: true),
-        body: _createBody(widget._configuration.parameters));
-  }
-
-  Widget _createBody(Map<String, Parameter> parameters) {
-    return GridView.count(
-      crossAxisCount: 2,
-      children: _createGridViewChildren(context, parameters),
+      appBar: AppBar(automaticallyImplyLeading: true),
+      body: ListView.builder(
+        itemCount: groupedParams.length,
+        itemBuilder: (context, i) => _listItemBuilder(
+            context, i, groupedParams, widget._configuration.parameters),
+      ),
     );
   }
 
-  List<Widget> _createGridViewChildren(
-      BuildContext context, Map<String, Parameter> parameters) {
-    var childWidgets = <Widget>[];
-    for (var name in parameters.keys) {
-      // Add parameter name
-      childWidgets.add(Text(name));
-      // Add input widget
-      childWidgets.add(_createInputWidget(context, parameters[name]));
-    }
-    return childWidgets;
-  }
-
-  Widget _createInputWidget(BuildContext context, Parameter parameter) {
-    Widget inputWidget;
-    var value = parameter.value;
-    var name = parameter.definition.name;
-    if (value is bool) {
-      inputWidget = DropdownButton(
-        value: value,
-        items: [false, true]
-            .map<DropdownMenuItem<bool>>(
-              (v) =>
-                  DropdownMenuItem<bool>(value: v, child: Text(v.toString())),
-            )
-            .toList(),
-        onChanged: (v) =>
-            setState(() => widget._configuration.setParameterValue(name, v)),
-      );
-    } else {
-      var defaultValue = parameter.definition.defaultValue;
-      // TODO: Add validation
-      inputWidget = TextField(
-        keyboardType:
-            (defaultValue is num ? TextInputType.number : TextInputType.text),
-        controller: getEditingControler(name, value.toString()),
-        decoration: InputDecoration(
-          labelText: '?',
-          errorText: _errorMessages[name],
-        ),
-        onChanged: (v) {
-          var errorMsg = _validate(v, parameter.definition.validators);
-          setState(() {
-            _errorMessages[name] = errorMsg;
-            if (errorMsg == null) {
-              widget._configuration
-                  .setParameterValue(name, _toProperType(v, defaultValue));
-            }
-          });
-        },
-      );
-    }
-    return inputWidget;
-  }
-
-  TextEditingController getEditingControler(String name, String initialValue) {
-    TextEditingController controller;
-    if (_editingControlers.containsKey(name)) {
-      controller = _editingControlers[name];
-    } else {
-      controller = TextEditingController(text: initialValue);
-      _editingControlers[name] = controller;
-    }
-    return controller;
-  }
-
-  dynamic _toProperType(String value, dynamic referenceType) {
-    dynamic typedValue;
-    if (value != null) {
-      if (referenceType is int) {
-        typedValue = int.parse(value);
-      } else if (referenceType is double) {
-        typedValue = double.parse(value);
-      } else if (referenceType is bool) {
-        typedValue = (value.toLowerCase() == 'true');
-      } else if (referenceType is String) {
-        typedValue = value;
+  /// List items contains: parameters, strings for headline
+  /// and nulls for dividers.
+  List<dynamic> _prepareListItems(Map<String, Parameter> parameters) {
+    // Divide parameters by sections
+    var groupedParams = <String, List<Parameter>>{};
+    for (var paramName in parameters.keys.toList()..sort()) {
+      var groupName = _extractGroupName(paramName);
+      if (groupedParams.containsKey(groupName)) {
+        groupedParams[groupName].add(parameters[paramName]);
+        groupedParams[groupName].sort((a,b) => a.definition.name.compareTo(b.definition.name));
       } else {
-        throw Exception(
-            'Not supported type ${referenceType?.runtimeType?.toString()}.');
+        groupedParams[groupName] = [parameters[paramName]];
       }
     }
-    return typedValue;
+
+    // Flat sections map to list separated with null
+    var i = 0;
+    var listItems = <dynamic>[];
+    for (var s in groupedParams.keys) {
+      // Add name of headline
+      listItems.add(s);
+      // Add parameters
+      listItems.addAll(groupedParams[s]);
+      if (i < groupedParams.length - 1) {
+        // Add parameters group separator
+        listItems.add(null);
+      }
+      i++;
+    }
+    return listItems;
   }
 
-  String _validate(String value, List<ParameterValidator> validators) {
-    var errorMsg;
-    for (var v in validators) {
-      var msg = v.validate(value);
-      if (msg != null && msg.isNotEmpty) {
-        if (errorMsg == null) {
-          errorMsg = msg;
-        } else {
-          errorMsg += ' $msg';
-        }
-        break;
-      }
+  // Extracts group name from parameter name [paramName]
+  String _extractGroupName(String paramName) {
+    var tokens = paramName?.split(sectionDivider);
+    var groupName;
+    if (tokens != null && tokens.length > 1) {
+      groupName = tokens[0];
     }
-    return errorMsg;
+    return groupName;
+  }
+
+  Widget _listItemBuilder(BuildContext context, int i, List<dynamic> listItems,
+      Map<String, Parameter> parameters) {
+    assert(i < listItems.length,
+        'Try to create list item with index $i, but there are ${listItems.length} items.');
+    var listItem = listItems[i];
+    var listItemWidget;
+    if (listItem is Parameter) {
+      listItemWidget = _createListItemForParameter(listItem);
+    } else if (listItem is String) {
+      listItemWidget = _createListItemForHeadline(listItem);
+    } else {
+      listItemWidget = Divider();
+    }
+    return listItemWidget;
+  }
+
+  Widget _createListItemForParameter(Parameter parameter) {
+    return ListTile(
+      title: Text('${parameter.definition.name}: ${parameter.value}'),
+    );
+  }
+
+  Widget _createListItemForHeadline(String headline) {
+    return ListTile(title: Text(headline));
   }
 }
