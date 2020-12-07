f5116484d981e6e94f8a1b0f1dc03404401398ae
tiptop <Tomasz.Gasior@gmail.com>
2020-12-08 00:11:08 +0100

    Added support for dynamic localization methods. Added new localization methods (translations).

--- lib/localizations.dart
+++ lib/localizations.dart
@@ -2,9 +2,17 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-
 import 'package:math_puzzles/l10n/messages_all.dart';
+import 'package:reflectable/reflectable.dart';
 
+class Reflector extends Reflectable {
+  // invokingCapability,
+  const Reflector() : super(declarationsCapability, instanceInvokeCapability);
+}
+
+const invokingReflector = Reflector();
+
+@invokingReflector
 class AppLocalizations {
   static Future<AppLocalizations> load(Locale locale) {
     var name =
@@ -48,6 +56,95 @@ class AppLocalizations {
 
   String get incorrectAnswersCount =>
       Intl.message('Incorrect answers count:', name: 'incorrectAnswersCount');
+
+  String get boolTrue => Intl.message('Yes', name: 'boolTrue');
+
+  String get boolFalse => Intl.message('No', name: 'boolFalse');
+
+  // Notice: dynamicMassage(...) can call just regular methods and not getters.
+
+  // Validators
+  String intTypeValidatorMsg() =>
+      Intl.message('Expected integer value.', name: 'intTypeValidatorMsg');
+
+  String boolTypeValidatorMsg() =>
+      Intl.message('Expected logic value - "true" or "false".',
+          name: 'boolTypeValidatorMsg');
+
+  String numScopeValidatorMsg(num minValue, num maxValue) =>
+      Intl.message('Allowed value between $minValue and ${maxValue}.',
+          name: 'numScopeValidatorMsg', args: [minValue, maxValue]);
+
+  // Puzzle generator parameters
+  // Addition Generator
+  String additionGenerator() =>
+      Intl.message('Addition', name: 'additionGenerator');
+
+  String additionGenerator_enabled() =>
+      Intl.message('Enabled', name: 'additionGenerator_enabled');
+
+  String additionGenerator_maxResult() =>
+      Intl.message('Max result', name: 'additionGenerator_maxResult');
+
+  String additionGenerator_fractionDigits() =>
+      Intl.message('Number of digits after the decimal point',
+          name: 'additionGenerator_fractionDigits');
+
+  // Percentage Generator
+  String percentageGenerator() =>
+      Intl.message('Percentage', name: 'percentageGenerator');
+
+  String percentageGenerator_enabled() =>
+      Intl.message('Enabled', name: 'percentageGenerator_enabled');
+
+  String percentageGenerator_maxResult() =>
+      Intl.message('Max result', name: 'percentageGenerator_maxResult');
+
+  String percentageGenerator_fractionDigits() =>
+      Intl.message('Number of digits after the decimal point',
+          name: 'percentageGenerator_fractionDigits');
+
+  // Multiplication Table Generator
+  String multiplicationTableGenerator() => Intl.message('Multiplication Table',
+      name: 'multiplicationTableGenerator');
+
+  String multiplicationTableGenerator_enabled() =>
+      Intl.message('Enabled', name: 'multiplicationTableGenerator_enabled');
+
+  String multiplicationTableGenerator_multiplicationTimes() =>
+      Intl.message('Multiplication times',
+          name: 'multiplicationTableGenerator_multiplicationTimes');
+
+  // General parameters
+  String session() => Intl.message('Session', name: 'session');
+
+  String session_puzzlesCount() =>
+      Intl.message('Number of puzzels', name: 'session_puzzlesCount');
+
+  String dynamicMessage(String name, {List<dynamic> args = const []}) {
+    var instanceMirror = invokingReflector.reflect(this);
+    var classMirror = instanceMirror.type;
+
+    // First of all check if requested method exists
+    var methodMirror;
+    var instanceMembers = classMirror.instanceMembers;
+    var methodName = name.replaceAll('\.', '_');
+    for (var memberName in instanceMembers.keys) {
+      if (memberName == methodName) {
+        methodMirror = instanceMembers[memberName];
+        break;
+      }
+    }
+
+    var message;
+    if (methodMirror != null) {
+      // If method exists - call it
+      message = instanceMirror.invoke(methodName, args);
+    } else {
+      message = '#$name#';
+    }
+    return message;
+  }
 }
 
 class AppLocalizationsDelegate extends LocalizationsDelegate<AppLocalizations> {
