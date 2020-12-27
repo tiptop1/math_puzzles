76c1649b63309ac7e951051377b1cdc335f495d8
t.gasior <t.gasior@crif.com>
2020-12-27 23:26:57 +0100

    Implemented session summary as 'donut pie chart' - work still in progress.

--- lib/gui/session_summary_route.dart
+++ lib/gui/session_summary_route.dart
@@ -1,3 +1,4 @@
+import 'package:charts_flutter/flutter.dart' as charts;
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:math_puzzles/gui/math_puzzle.dart';
@@ -12,36 +13,83 @@ class SessionSummaryRoute extends StatelessWidget {
     var _sessionModel =
         ModalRoute.of(context).settings.arguments as SessionModel;
     return Scaffold(
-        appBar: AppBar(
-          title: Text(AppLocalizations.of(context).sessionSummary),
-          leading: null,
-          automaticallyImplyLeading: false,
-          actions: createActions(context),
+      appBar: AppBar(
+        title: Text(AppLocalizations.of(context).sessionSummary),
+        leading: null,
+        automaticallyImplyLeading: false,
+        actions: createActions(context),
+      ),
+      body: Container(
+        child: Center(
+          child: charts.PieChart(
+            _createSeriesList(context, _sessionModel),
+            animate: false,
+            defaultRenderer: charts.ArcRendererConfig(
+              arcWidth: 120,
+              // new code below
+              arcRendererDecorators: [
+                charts.ArcLabelDecorator(
+                  showLeaderLines: false,
+                  // outsideLabelStyleSpec: charts.TextStyleSpec(fontSize: 18),
+                  insideLabelStyleSpec: charts.TextStyleSpec(fontSize: 18),
+                  labelPosition: charts.ArcLabelPosition.inside,
+                )
+              ],
+            ),
+            behaviors: [
+              // our title behaviour
+              charts.DatumLegend(
+                position: charts.BehaviorPosition.bottom,
+                outsideJustification:
+                    charts.OutsideJustification.middleDrawArea,
+                horizontalFirst: false,
+                cellPadding: EdgeInsets.only(right: 4.0, bottom: 4.0),
+                showMeasures: true,
+                desiredMaxColumns: 2,
+                desiredMaxRows: 2,
+                legendDefaultMeasure: charts.LegendDefaultMeasure.firstValue,
+                measureFormatter: (num value) => '',
+                entryTextStyle: charts.TextStyleSpec(
+                    color: charts.MaterialPalette.black,
+                    fontFamily: 'Roboto',
+                    fontSize: 16),
+              ),
+            ],
+          ),
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Center(
-              child: _createCorrectAnswersSummary(
-                  context, _sessionModel.correctAnswersCount),
-            ),
-            Center(
-              child: _createIncorrectAnswersSummary(
-                  context, _sessionModel.incorrectAnswersCount),
-            ),
-          ],
-        ));
+      ),
+    );
   }
 
-  Widget _createCorrectAnswersSummary(
-      BuildContext context, int correctAnswersCount) {
-    return Text(
-        '${AppLocalizations.of(context).correctAnswersCount} $correctAnswersCount');
+  List<charts.Series> _createSeriesList(
+      BuildContext context, SessionModel sessionModel) {
+    return [
+      charts.Series<AnswersCount, String>(
+        id: 'AnswersCount',
+        domainFn: (AnswersCount answersCount, _) => answersCount.category,
+        measureFn: (AnswersCount answersCount, _) => answersCount.count,
+        colorFn: (AnswersCount answersCount, _) => answersCount.color,
+        labelAccessorFn: (AnswersCount answersCount, _) =>
+            answersCount.count.toString(),
+        data: [
+          AnswersCount(
+              sessionModel.correctAnswersCount,
+              AppLocalizations.of(context).correctAnswers,
+              charts.ColorUtil.fromDartColor(Colors.green)),
+          AnswersCount(
+              sessionModel.incorrectAnswersCount,
+              AppLocalizations.of(context).incorrectAnswers,
+              charts.ColorUtil.fromDartColor(Colors.red)),
+        ],
+      )
+    ];
   }
+}
 
-  Widget _createIncorrectAnswersSummary(
-      BuildContext context, int incorrectAnswersCount) {
-    return Text(
-        '${AppLocalizations.of(context).incorrectAnswersCount} $incorrectAnswersCount');
-  }
+class AnswersCount {
+  final int count;
+  final String category;
+  final charts.Color color;
+
+  AnswersCount(this.count, this.category, this.color);
 }
