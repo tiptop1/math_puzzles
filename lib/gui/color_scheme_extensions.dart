bd71348e493b5e0561b18eb9dca92b1aa1259d12
t.gasior <t.gasior@crif.com>
2020-12-29 23:53:08 +0100

    Extended Material color scheme.

---
+++ lib/gui/color_scheme_extensions.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+extension CustomColorScheme on ColorScheme {
+  Color get correctAnswer => Colors.green;
+
+  Color get incorrectAnswer => Colors.red;
+
+  Color get answer => Colors.blue;
+}
