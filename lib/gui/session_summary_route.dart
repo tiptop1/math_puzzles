f9a24490f00da3b78c9be52fabab7586a9c9bc4e
tiptop <Tomasz.Gasior@gmail.com>
2021-05-21 12:48:00 +0200

    Replace charts_flutter with fl_chart library (more flexible, null safety, seems to be better :).

--- lib/gui/session_summary_route.dart
+++ lib/gui/session_summary_route.dart
@@ -1,6 +1,4 @@
-import 'dart:math';
-
-import 'package:charts_flutter/flutter.dart' as charts;
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:math_puzzles/generated/l10n.dart';
@@ -12,84 +10,115 @@ import 'color_scheme_extensions.dart';
 class SessionSummaryRoute extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var sessionModel =
-        ModalRoute.of(context)!.settings.arguments as SessionModel;
-    var totalAnswersCount =
-        sessionModel.correctAnswersCount + sessionModel.incorrectAnswersCount;
+    var screenSize = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(
-        title: Text(AppLocalizations.of(context).sessionSummaryTitle),
-        leading: null,
-        automaticallyImplyLeading: false,
-        actions: createActions(context),
-      ),
-      body: Container(
-        margin: const EdgeInsets.all(50.0),
-        child: charts.PieChart(
-          _createSeriesList(context, sessionModel),
-          animate: false,
-          defaultRenderer: charts.ArcRendererConfig(
-            arcWidth: 30,
-            startAngle: 4 / 5 * pi,
-            arcLength: 7 / 5 * pi,
-          ),
-          behaviors: [
-            // our title behaviour
-            charts.DatumLegend(
-              position: charts.BehaviorPosition.bottom,
-              outsideJustification: charts.OutsideJustification.middleDrawArea,
-              horizontalFirst: false,
-              cellPadding: EdgeInsets.only(right: 4.0, bottom: 4.0),
-              showMeasures: true,
-              desiredMaxColumns: 2,
-              desiredMaxRows: 2,
-              legendDefaultMeasure: charts.LegendDefaultMeasure.firstValue,
-              measureFormatter: (num value) =>
-                  '$value (${_calculatePercent(value, totalAnswersCount)}%)',
-
-              entryTextStyle: charts.TextStyleSpec(
-                  fontFamily: 'Roboto',
-                  fontSize: 20),
+        appBar: AppBar(
+          title: Text(AppLocalizations.of(context).sessionSummaryTitle),
+          leading: null,
+          automaticallyImplyLeading: false,
+          actions: createActions(context),
+        ),
+        body: Flex(
+          direction: screenSize.width > screenSize.height
+              ? Axis.horizontal
+              : Axis.vertical,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+              flex: 3,
+              child: PieChart(
+                PieChartData(
+                    pieTouchData: PieTouchData(
+                      enabled: false,
+                    ),
+                    borderData: FlBorderData(
+                      show: false,
+                    ),
+                    sectionsSpace: 0,
+                    centerSpaceRadius: 100.0,
+                    sections: _createSections(context)),
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: _createLegend(context),
             ),
           ],
-        ),
-      ),
-    );
+        ));
   }
 
-  List<charts.Series> _createSeriesList(
-      BuildContext context, SessionModel sessionModel) {
-    return [
-      charts.Series<AnswersCount, String>(
-        id: 'AnswersCount',
-        domainFn: (AnswersCount answersCount, _) => answersCount.category,
-        measureFn: (AnswersCount answersCount, _) => answersCount.count,
-        colorFn: (AnswersCount answersCount, _) => answersCount.color,
-        data: [
-          AnswersCount(
-              sessionModel.correctAnswersCount,
-              '${AppLocalizations.of(context).correctAnswers}:',
-              charts.ColorUtil.fromDartColor(
-                  Theme.of(context).colorScheme.correctAnswer)),
-          AnswersCount(
-              sessionModel.incorrectAnswersCount,
-              '${AppLocalizations.of(context).incorrectAnswers}:',
-              charts.ColorUtil.fromDartColor(
-                  Theme.of(context).colorScheme.incorrectAnswer)),
-        ],
-      )
-    ];
+  List<PieChartSectionData> _createSections(BuildContext context) {
+    var sessionModel =
+        ModalRoute.of(context)!.settings.arguments as SessionModel;
+    var correctAnswersCount = sessionModel.correctAnswersCount;
+    var incorrectAnswersCount = sessionModel.incorrectAnswersCount;
+    var totalAnswersCount = correctAnswersCount + incorrectAnswersCount;
+    return List.generate(2, (i) {
+      switch (i) {
+        case 0:
+          return PieChartSectionData(
+            color: Theme.of(context).colorScheme.correctAnswer,
+            value: correctAnswersCount.toDouble(),
+            title:
+                '$correctAnswersCount (${_calculatePercent(correctAnswersCount, totalAnswersCount)}%)',
+            radius: 60.0,
+          );
+        case 1:
+          return PieChartSectionData(
+            color: Theme.of(context).colorScheme.incorrectAnswer,
+            value: incorrectAnswersCount.toDouble(),
+            title:
+                '$incorrectAnswersCount (${_calculatePercent(incorrectAnswersCount, totalAnswersCount)}%)',
+            radius: 60.0,
+          );
+        default:
+          throw Error();
+      }
+    });
   }
 
   int _calculatePercent(num value, num wholeValue) {
     return ((value * 100) / wholeValue).round();
   }
+
+  Widget _createLegend(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(10.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          LegendItem(AppLocalizations.of(context).correctAnswers,
+              Theme.of(context).colorScheme.correctAnswer),
+          SizedBox(height: 5),
+          LegendItem(AppLocalizations.of(context).incorrectAnswers,
+              Theme.of(context).colorScheme.incorrectAnswer),
+        ],
+      ),
+    );
+  }
 }
 
-class AnswersCount {
-  final int count;
-  final String category;
-  final charts.Color color;
+class LegendItem extends StatelessWidget {
+  static const double size = 20;
+  static const double textGap = 4;
+  final String _text;
+  final Color _color;
 
-  AnswersCount(this.count, this.category, this.color);
+  const LegendItem(this._text, this._color);
+
+  @override
+  Widget build(BuildContext buildContext) {
+    return Row(children: [
+      Container(
+        color: _color,
+        width: size,
+        height: size,
+      ),
+      SizedBox(
+        width: textGap,
+      ),
+      Text(_text),
+    ]);
+  }
 }
