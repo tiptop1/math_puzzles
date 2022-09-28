7744ea751f08a569f3ba282ba09ee112b54ef2c9
tiptop <Tomasz.Gasior@gmail.com>
2022-09-28 23:44:13 +0200

    Do not use ParametrizedMessage any longer - directly translate validation messages in validateValue(...) method.

--- lib/gui/parameter_definition/parameter_definition.dart
+++ lib/gui/parameter_definition/parameter_definition.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:math_puzzles/gui/parameter_definition/parameter_validators.dart';
 
 import 'parameter_converters.dart';
@@ -10,6 +11,7 @@ abstract class ParameterDefinition {
 
 class GroupParameterDefinition extends ParameterDefinition {
   final List<ValueParameterDefinition> children;
+
   const GroupParameterDefinition(String name, this.children) : super(name);
 }
 
@@ -17,32 +19,23 @@ class ValueParameterDefinition<T extends Object> extends ParameterDefinition {
   final ValueConverter<T> converter;
   final List<ParameterValidator<T>> validators;
 
-  const ValueParameterDefinition(String name, this.converter, {this.validators = const []}) : super(name);
+  const ValueParameterDefinition(String name, this.converter,
+      {this.validators = const []})
+      : super(name);
 
   /// If returns null, conversion from string failed.
-  T? convertValue(String strValue) => converter.convertValue(strValue);
+  T? convertValue(String? strValue) => converter.convertValue(strValue);
 
   Type getExpectedType() => T.runtimeType;
 
-  ParametrizedMessage? validateValue(T? value) {
-    var parametrizedMsg;
-    if (value == null) {
-      var expectedType = getExpectedType();
-      if (expectedType == int) {
-        parametrizedMsg = ParametrizedMessage('intTypeValidator');
-      } else if (expectedType == bool) {
-        parametrizedMsg = ParametrizedMessage('boolTypeValidator');
-      } else {
-        throw UnsupportedError('Parameter of type $expectedType not supported.');
-      }
-    } else {
-      for (var v in validators) {
-        parametrizedMsg = v.validate(value);
-        if (parametrizedMsg != null) {
-          break;
-        }
+  String? validateValue(BuildContext context, T value) {
+    var validationMsg;
+    for (var v in validators) {
+      validationMsg = v.validate(context, value);
+      if (validationMsg != null) {
+        break;
       }
     }
-    return parametrizedMsg;
+    return validationMsg;
   }
 }
