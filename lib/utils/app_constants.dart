bc33b21788121c9c54b06ea89801830ad5c0d100
tiptop <Tomasz.Gasior@gmail.com>
2022-10-07 10:40:10 +0200

    Format the file.

--- lib/utils/app_constants.dart
+++ lib/utils/app_constants.dart
@@ -14,30 +14,71 @@ class AppConstants {
   // TODO: Parameter definitions - it should be moved in better place.
   static List<ParameterDefinition> parameterDefinitions = [
     GroupParameterDefinition(Configuration.sesstionGroup, [
-      ValueParameterDefinition<int>(Configuration.sessionsPuzzlesCountParam, IntValueConverter(), validators: [IntParameterScopeValidator(1, 100),],),
+      ValueParameterDefinition<int>(
+        Configuration.sessionsPuzzlesCountParam,
+        IntValueConverter(),
+        validators: [
+          IntParameterScopeValidator(1, 100),
+        ],
+      ),
     ]),
     GroupParameterDefinition(Configuration.additionGroup, [
-      ValueParameterDefinition(Configuration.additionEnabledParam, BoolValueConverter()),
-      ValueParameterDefinition(Configuration.additionMaxResultParam, IntValueConverter(), validators: [IntParameterScopeValidator(1, 10000)]),
-      ValueParameterDefinition(Configuration.additionFractionDigits, IntValueConverter(), validators: [IntParameterScopeValidator(0, 4)],),
+      ValueParameterDefinition(
+          Configuration.additionEnabledParam, BoolValueConverter()),
+      ValueParameterDefinition(
+          Configuration.additionMaxResultParam, IntValueConverter(),
+          validators: [IntParameterScopeValidator(1, 10000)]),
+      ValueParameterDefinition(
+        Configuration.additionFractionDigits,
+        IntValueConverter(),
+        validators: [IntParameterScopeValidator(0, 4)],
+      ),
     ]),
     GroupParameterDefinition(Configuration.subtractionGroup, [
-      ValueParameterDefinition(Configuration.subtractionEnabledParam, BoolValueConverter()),
-      ValueParameterDefinition(Configuration.subtractionMaxResultParam, IntValueConverter(), validators: [IntParameterScopeValidator(1, 10000)],),
-      ValueParameterDefinition(Configuration.subtractionFractionDigitsParam, IntValueConverter(), validators: [IntParameterScopeValidator(0, 4)],),
+      ValueParameterDefinition(
+          Configuration.subtractionEnabledParam, BoolValueConverter()),
+      ValueParameterDefinition(
+        Configuration.subtractionMaxResultParam,
+        IntValueConverter(),
+        validators: [IntParameterScopeValidator(1, 10000)],
+      ),
+      ValueParameterDefinition(
+        Configuration.subtractionFractionDigitsParam,
+        IntValueConverter(),
+        validators: [IntParameterScopeValidator(0, 4)],
+      ),
     ]),
     GroupParameterDefinition(Configuration.multiplicationTableGroup, [
-      ValueParameterDefinition(Configuration.multiplicationTableEnabledParam, BoolValueConverter()),
-      ValueParameterDefinition(Configuration.multiplicationTableTimesParam, IntValueConverter(), validators: [IntParameterScopeValidator(10, 1000)],),
+      ValueParameterDefinition(
+          Configuration.multiplicationTableEnabledParam, BoolValueConverter()),
+      ValueParameterDefinition(
+        Configuration.multiplicationTableTimesParam,
+        IntValueConverter(),
+        validators: [IntParameterScopeValidator(10, 1000)],
+      ),
     ]),
     GroupParameterDefinition(Configuration.divisionGroup, [
-      ValueParameterDefinition(Configuration.divisionEnabledParam, BoolValueConverter()),
-      ValueParameterDefinition(Configuration.divisionMaxResultParam, IntValueConverter(), validators: [IntParameterScopeValidator(10, 1000)],)
+      ValueParameterDefinition(
+          Configuration.divisionEnabledParam, BoolValueConverter()),
+      ValueParameterDefinition(
+        Configuration.divisionMaxResultParam,
+        IntValueConverter(),
+        validators: [IntParameterScopeValidator(10, 1000)],
+      )
     ]),
     GroupParameterDefinition(Configuration.percentageGroup, [
-      ValueParameterDefinition(Configuration.percentageEnabledParam, BoolValueConverter()),
-      ValueParameterDefinition(Configuration.percentageMaxResultParam, IntValueConverter(), validators: [IntParameterScopeValidator(1, 1000)],),
-      ValueParameterDefinition(Configuration.percentageFractionDigitsParam, IntValueConverter(), validators: [IntParameterScopeValidator(0, 4)],),
+      ValueParameterDefinition(
+          Configuration.percentageEnabledParam, BoolValueConverter()),
+      ValueParameterDefinition(
+        Configuration.percentageMaxResultParam,
+        IntValueConverter(),
+        validators: [IntParameterScopeValidator(1, 1000)],
+      ),
+      ValueParameterDefinition(
+        Configuration.percentageFractionDigitsParam,
+        IntValueConverter(),
+        validators: [IntParameterScopeValidator(0, 4)],
+      ),
     ]),
   ];
 }
